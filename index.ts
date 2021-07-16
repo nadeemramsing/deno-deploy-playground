b7ae12641f6a5f563317805cea52c529b61a6017
Nadeem Ramsing <nadeem.ramsing@gmail.com>
2021-07-16 22:58:21 +0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,9 +1,14 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+
+const router = new Router();
+
+router.get("/hello/:name", (ctx) => {
+  ctx.response.body = `Hello, ${ctx.params?.name ?? 'Nadeem'}`;
+})
 
 const app = new Application();
 
-app.use((ctx) => {
-  ctx.response.body = "Hello World!";
-});
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 addEventListener("fetch", app.fetchEventHandler());
