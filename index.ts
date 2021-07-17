683963f8218fca13f41553d61ff1eaf2d8fd6fc7
Nadeem Ramsing <nadeem.ramsing@chb-technologies.ch>
2021-07-17 10:30:12 +0400

    Further routes added

--- index.ts
+++ index.ts
@@ -3,7 +3,15 @@ import { Application, Router } from "https://deno.land/x/oak/mod.ts";
 const router = new Router();
 
 router.get("/hello/:name", (ctx) => {
-  ctx.response.body = `Hello, ${ctx.params?.name ?? 'Nadeem'}`;
+  ctx.response.body = `Hello, ${ctx.params?.name ?? 'Nadeem'}.`
+})
+
+router.get("/hello", (ctx) => {
+  ctx.response.body = 'Hello, World.';
+})
+
+router.get("/", (ctx) => {
+  ctx.response.body = 'Welcome to deno-deploy-playground.';
 })
 
 const app = new Application();
